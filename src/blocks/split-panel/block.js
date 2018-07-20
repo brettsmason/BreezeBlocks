36b0d976068c37d8071bcaeebd2eb9ec7fd8e76b
Brett Mason <brettsmason@gmail.com>
2018-07-20 13:45:38 +0100

    Add polish to split panel

--- src/blocks/split-panel/block.js
+++ src/blocks/split-panel/block.js
@@ -15,12 +15,15 @@ import classnames from 'classnames';
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
 const {
+	getColorClass,
 	MediaPlaceholder,
 	MediaUpload,
 	InnerBlocks,
 	BlockControls,
 	BlockAlignmentToolbar,
 	InspectorControls,
+	PanelColor,
+	withColors,
 } = wp.editor;
 const { Fragment } = wp.element;
 const {
@@ -44,9 +47,9 @@ const {
  * @return {?WPBlock}          The block, if it has been successfully
  *                             registered; otherwise `undefined`.
  */
-registerBlockType( 'breezeblocks/block-split-panel', {
+registerBlockType( 'breezeblocks/split-panel', {
 	title: __( 'Split Panel' ),
-	icon: 'image-flip-horizontal',
+	icon: 'columns',
 	category: 'layout',
 	keywords: [
 		__( 'split panel' ),
@@ -65,6 +68,12 @@ registerBlockType( 'breezeblocks/block-split-panel', {
 		id: {
 			type: 'number',
 		},
+		backgroundColor: {
+			type: 'string',
+		},
+		textColor: {
+			type: 'string',
+		},
 	},
 
 	getEditWrapperProps( attributes ) {
@@ -82,106 +91,124 @@ registerBlockType( 'breezeblocks/block-split-panel', {
 	 *
 	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
 	 */
-	edit( { attributes, setAttributes, className } ) {
-		const { align, id, url, reversed } = attributes;
+	edit: withColors( 'backgroundColor', 'textColor' )(
+		function( props ) {
+			const { attributes: { align, reversed, url, id }, className, setAttributes, backgroundColor, textColor, setBackgroundColor, setTextColor } = props;
 
-		const classes = classnames(
-			className,
-			'split-panel',
-			reversed ? 'split-panel--reversed' : null,
-			{ [ `align${ align }` ]: align }
-		);
+			const classes = classnames( className, 'split-panel', {
+				'split-panel--reversed' : reversed,
+				'has-background': backgroundColor.value,
+				[ backgroundColor.class ]: backgroundColor.class,
+				[ textColor.class ]: textColor.class,
+				[ `align${ align }` ]: align,
+			} );
 
-		const updateAlignment = ( nextAlign ) => setAttributes( { align: nextAlign } );
+			const updateAlignment = ( nextAlign ) => setAttributes( { align: nextAlign } );
 
-		const toggleReversed = () => setAttributes( { reversed: ! reversed } );
+			const toggleReversed = () => setAttributes( { reversed: ! reversed } );
 
-		const onSelectImage = ( media ) => {
-			if ( ! media || ! media.url ) {
-				setAttributes( { url: undefined, id: undefined } );
-				return;
-			}
-			setAttributes( { url: media.url, id: media.id } );
-		};
+			const onSelectImage = ( media ) => {
+				if ( ! media || ! media.url ) {
+					setAttributes( { url: undefined, id: undefined } );
+					return;
+				}
+				setAttributes( { url: media.url, id: media.id } );
+			};
 
-		return (
-			<Fragment>
-				<InspectorControls>
-					<PanelBody>
-						<PanelRow>
-							<label
-								htmlFor="reversed-form-toggle"
-								className="blocks-base-control__label"
-							>
-								{ __( 'Reverse Layout' ) }
-							</label>
-							<FormToggle
-								id="reversed-form-toggle"
-								label={ __( 'Reverse Layout' ) }
-								checked={ !! reversed }
-								onChange={ toggleReversed }
-							/>
-						</PanelRow>
-					</PanelBody>
-				</InspectorControls>
-
-				<BlockControls>
-					<BlockAlignmentToolbar
-						value={ align }
-						onChange={ updateAlignment }
-						controls={ [ 'wide', 'full' ] }
-					/>
-
-					<Toolbar>
-						<MediaUpload
-							onSelect={ onSelectImage }
-							type="image"
-							value={ id }
-							render={ ( { open } ) => (
-								<IconButton
-									className="components-toolbar__control"
-									label={ __( 'Edit image' ) }
-									icon="edit"
-									onClick={ open }
+			return (
+				<Fragment>
+					<InspectorControls>
+						<PanelBody>
+							<PanelRow>
+								<label
+									htmlFor="reversed-form-toggle"
+									className="blocks-base-control__label"
+								>
+									{ __( 'Reverse Layout' ) }
+								</label>
+								<FormToggle
+									id="reversed-form-toggle"
+									label={ __( 'Reverse Layout' ) }
+									checked={ !! reversed }
+									onChange={ toggleReversed }
 								/>
-							) }
+							</PanelRow>
+						</PanelBody>
+						<PanelColor
+							colorValue={ backgroundColor.value }
+							title={ __( 'Background Color' ) }
+							onChange={ setBackgroundColor }
 						/>
-					</Toolbar>
-				</BlockControls>
+						<PanelColor
+							colorValue={ textColor.value }
+							title={ __( 'Text Color' ) }
+							onChange={ setTextColor }
+						/>
+					</InspectorControls>
 
-				<div className={ classes }>
-					<div
-						data-url={ url }
-						className={ 'split-panel__image' }
-						style={ { backgroundImage: 'url(' + url + ')' } }
-					>
-						{
-							! url && (
-								<MediaPlaceholder
-									icon="format-image"
-									labels={ {
-										title: __( 'Image' ),
-										name: __( 'an image' ),
-									} }
-									className={ className }
-									onSelect={ onSelectImage }
-									accept="image/*"
-									type="image"
-								/>
-							)
-						}
-					</div>
-					<div className={ 'split-panel__box' }>
-						<div className={ 'split-panel__content' }>
-							<InnerBlocks
-								allowedBlocks={ [ 'core/heading', 'core/paragraph', 'core/button' ] }
+					<BlockControls>
+						<BlockAlignmentToolbar
+							value={ align }
+							onChange={ updateAlignment }
+							controls={ [ 'wide', 'full' ] }
+						/>
+
+						<Toolbar>
+							<MediaUpload
+								onSelect={ onSelectImage }
+								type="image"
+								value={ id }
+								render={ ( { open } ) => (
+									<IconButton
+										className="components-toolbar__control"
+										label={ __( 'Edit image' ) }
+										icon="edit"
+										onClick={ open }
+									/>
+								) }
 							/>
+						</Toolbar>
+					</BlockControls>
+
+					<div className={ classes }>
+						<div
+							data-url={ url }
+							className={ 'split-panel__image' }
+							style={ { backgroundImage: 'url(' + url + ')' } }
+						>
+							{
+								! url && (
+									<MediaPlaceholder
+										icon="format-image"
+										labels={ {
+											title: __( 'Image' ),
+											name: __( 'an image' ),
+										} }
+										className={ className }
+										onSelect={ onSelectImage }
+										accept="image/*"
+										type="image"
+									/>
+								)
+							}
+						</div>
+						<div className={ 'split-panel__box' }>
+							<div className={ 'split-panel__content' }>
+								<InnerBlocks
+									allowedBlocks={ [ 'core/heading', 'core/paragraph', 'core/button', 'core/list' ] }
+									template={ [
+										[ 'core/heading' ],
+										[ 'core/paragraph' ],
+									] }
+									templateLock={ false }
+								/>
+							</div>
 						</div>
 					</div>
-				</div>
-			</Fragment>
-		);
-	},
+				</Fragment>
+			);
+		}
+	),
 
 	/**
 	 * The save function defines the way in which the different attributes should be combined
@@ -192,20 +219,23 @@ registerBlockType( 'breezeblocks/block-split-panel', {
 	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
 	 */
 	save: function( props ) {
-		const { align } = props.attributes;
+		const { attributes: { align, reversed, url, backgroundColor, textColor }, className } = props;
 
-		const classes = classnames(
-			props.className,
-			'split-panel',
-			props.attributes.reversed ? 'split-panel--reversed' : null,
-			{ [ `align${ align }` ]: align }
-		);
+		const backgroundClass = getColorClass( 'background-color', backgroundColor );
+		const textClass = getColorClass( 'color', textColor );
+		const classes = classnames( className, 'split-panel', {
+			'split-panel--reversed' : reversed,
+			[ `align${ align }` ]: align,
+			'has-background': backgroundColor.value,
+			[ backgroundClass ]: backgroundClass,
+			[ textClass ]: textClass,
+		} );
 
 		return (
 			<div className={ classes }>
 				<div
 					className={ 'split-panel__image' }
-					style={ { backgroundImage: 'url(' + props.attributes.url + ')' } }
+					style={ { backgroundImage: 'url(' + url + ')' } }
 				>
 				</div>
 				<div className={ 'split-panel__box' }>
